825e334dedd4a6e653fb3caf800504ef5ca9922f
RagdollGoDown <86296692+RagdollGoDown@users.noreply.github.com>
2023-08-20 23:24:39 +0200

    update MeleeWeapon.cs

---
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastGunBehaviour : WeaponBehaviour
+{
+    private float chargeTime;
+    private float cooldownTime;
+    
+    private void Awake()
+    {
+        AwakeWeapon();
+    }
+    
+    protected override void AwakeWeapon();
+    
+    public override void 
+}
