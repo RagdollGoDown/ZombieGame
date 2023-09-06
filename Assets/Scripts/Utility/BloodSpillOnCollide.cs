cffff7a421655cbbf364f1d069768ab863d3fc19
RagdollGoDown <zenooby2002@gmail.com>
2023-09-06 13:45:01 +0200

    Blood Spill change
    blood spill :
    -is a sprite not a decal for asthetic reasons
    - have a scale variable

--- Assets/Scripts/Utility/BloodSpillOnCollide.cs
+++ Assets/Scripts/Utility/BloodSpillOnCollide.cs
@@ -1,27 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Collider))]
 public class BloodSpillOnCollide : MonoBehaviour
 {
     private static readonly float BLOODSPILL_LIFETIME = 10;
-    [SerializeField] private GameObject bloodSpillPrefab;
+
+    private static int BLOOD_SPILLS_INDEX;
+
+    [SerializeField] private GameObject[] bloodSpillPrefabs;
     [SerializeField] private bool destroyOnCollide;
+    [SerializeField] private float scale;
 
     private void OnCollisionEnter(Collision collision)
     {
         //only bloodsplat on default surfaces
         if (collision.gameObject.layer != 0) { return; }
 
-        if (bloodSpillPrefab)
+        if (bloodSpillPrefabs == null || bloodSpillPrefabs.Length == 0) { return; }
+
+        GameObject bloodSpill = bloodSpillPrefabs[BLOOD_SPILLS_INDEX++ % bloodSpillPrefabs.Length];
+
+        if (bloodSpill)
         {
-            Transform BSP = Instantiate(bloodSpillPrefab, collision.contacts[0].point, Quaternion.identity).transform;
+            Transform BSP = Instantiate(bloodSpill, collision.contacts[0].point, Quaternion.identity).transform;
             BSP.rotation = Quaternion.FromToRotation(-BSP.forward, collision.GetContact(0).normal);
             BSP.position += -BSP.forward * 0.02f;
+            BSP.localScale *= scale;
             Destroy(BSP.gameObject, BLOODSPILL_LIFETIME);
         }
 
